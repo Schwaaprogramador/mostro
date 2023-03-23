267ccd40f1a946fad71f08b8311022a58edbf90c
Schwaaprogramador <danieltoro.programacion@gmail.com>
2023-03-22 22:01:55 -0500

    nevagtion fixes

--- mostro-client/src/components/Culture/Culture.jsx
+++ mostro-client/src/components/Culture/Culture.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import styled from './Culture.module.css'
+import React from 'react';
+import styled from './Culture.module.css';
+import img from '../../assets/img/img2.jpg';
 
 function Culture() {
   return (
     <div className={styled.container}>
-      <h3>Culture</h3>
+
+      <div className={styled.center}>
+            <h2>Culture</h2>
+            <img src={img} alt="medellin" className={styled.img}/>
+      </div>
+      
     </div>
   )
 }
