a372990fd0ce0b5dac7d31efa544befc85bc098d
Schwaaprogramador <danieltoro.programacion@gmail.com>
2023-03-30 16:36:31 -0500

    Store animation

--- mostro-client/src/components/Store/Store.jsx
+++ mostro-client/src/components/Store/Store.jsx
@@ -1,48 +1,65 @@
 import React from 'react';
 import styled from './Store.module.css';
-import img from '../../assets/img/imagen2.jpg';
-import img2 from '../../assets/img/imagen3.jpg';
-import img3 from '../../assets/img/imagen4.jpg';
-import img4 from '../../assets/img/imagen5.jpg';
+import Navbar from '../Navbar/Navbar';  
+import {useSelector, useDispatch  } from "react-redux";
+import { useEffect } from 'react';
+import { showNavbar, closeNavbar } from '../../redux/navbarSlice.js'; 
+import { CgChevronRightR } from "react-icons/cg"; 
 
 function Store() {
+
+  const dispatch = useDispatch();
+  const navbarShow = useSelector(state => state.navbarStatus);
+
+  useEffect(()=>{
+    dispatch(closeNavbar());
+    
+  },[dispatch]);
+
+
   return (
     <div className={styled.container}>
 
-      <h2>STORE</h2>
+      <div>
+      { navbarShow.status ? <Navbar/> : null }
+        <button onClick={() => dispatch(showNavbar())} className={styled.ham}>
+
+          { navbarShow.status ? null : <CgChevronRightR className={styled.icon} /> }
+                              
+        </button>
+
+      </div>
       
-      {/* <div className={styled.item}>
-        <img src={img} alt="medellin"  className={styled.img}/>
-        <h3>Imagen 1</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, 
-          nemo! Necessitatibus, quod reiciendis! Voluptate itaque velit molestias labore 
-          a qui nobis praesentium est id, ad quas suscipit temporibus incidunt omnis.</p>
+      
+
+      <div className={styled.center}>
+      
+       
+
+        <div className={styled.img}>
+          <div className={styled.imgInfo}>
+
+            <h3>Camisa Chimbera</h3>
+                <div>
+                  <p>Talla: La que le sirve</p>
+                  <p>Precio: cuanto tiene</p>
+                </div>
+            
+          </div>
+        </div>
+
+        <div className={styled.img2}>
+          <div className={styled.imgInfo}>
+            <h3><span>Camisa</span> Chimbera pa parchar</h3>
+          </div>
+        </div>
+   
+        
+
+
       </div>
-
-      <div className={styled.item}>
-        <img src={img2} alt="medellin" className={styled.img}/>
-        <h3>Imagen 2</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, 
-          nemo! Necessitatibus, quod reiciendis! Voluptate itaque velit molestias labore 
-          a qui nobis praesentium est id, ad quas suscipit temporibus incidunt omnis.</p>
-      </div>
-
-      <div className={styled.item}>
-        <img src={img3} alt="medellin" className={styled.img} />
-        <h3>Imagen 3</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, 
-          nemo! Necessitatibus, quod reiciendis! Voluptate itaque velit molestias labore 
-          a qui nobis praesentium est id, ad quas suscipit temporibus incidunt omnis.</p>
-      </div>
-
-      <div className={styled.item}>
-        <img src={img4} alt="medellin"  className={styled.img}/>
-        <h3>Imagen 4</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, 
-          nemo! Necessitatibus, quod reiciendis! Voluptate itaque velit molestias labore 
-          a qui nobis praesentium est id, ad quas suscipit temporibus incidunt omnis.</p>
-      </div> */}
-
+      
+      
 
 
 
